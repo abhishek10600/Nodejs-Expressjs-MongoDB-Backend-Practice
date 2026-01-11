621d61614810e4e54f47a0cc1a9e38ce6771b257
abhishek10600 <abhi10sheksharma@gmail.com>
2026-01-11 18:39:57 +0530

    AsyncHandler, ApiError and ApiResponse files created in utils.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,23 @@ dotenv.config({
 });
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+const port = process.env.PORT || 8001;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Server Error: ", error);
+      throw error;
+    });
+    app.listen(port, () => {
+      console.log(`Server running on PORT ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Failed: ", error);
+  });
 
 /*
 
