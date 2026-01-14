b885c53224b05dd97f45054e9f4e107f5790e696
abhishek10600 <abhi10sheksharma@gmail.com>
2026-01-14 18:08:47 +0530

    Auth middleware created. Login and Logout created.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -156,4 +156,124 @@ const registerUser = async (req, res) => {
   }
 };
 
-export { registerUser };
+// 1. Get data from req.body (username or emai and password)
+// 2. Find the user
+// 3. Check the password
+// 4. access and refersh token
+const loginUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username && !email) {
+      throw new ApiError(400, "username or email is required.");
+    }
+
+    if (!password) {
+      throw new ApiError(400, "password is required");
+    }
+
+    const user = await User.findOne({
+      $or: [{ username }, { email }],
+    });
+
+    if (!user) {
+      throw new ApiError(404, "user does not exists");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(
+      password,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      throw new ApiError(401, "invalid user credentials");
+    }
+
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    const loggedInUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+
+    const cookiesOptions = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, cookiesOptions)
+      .cookie("refreshToken", refreshToken, cookiesOptions)
+      .json(
+        new ApiResponse(
+          200,
+          { success: true, user: loggedInUser, accessToken, refreshToken },
+          "user logged in successfully"
+        )
+      );
+  } catch (error) {
+    console.error("Error: ", error);
+
+    const statusCode = error instanceof ApiError ? error.statusCode : 500;
+
+    const message =
+      error instanceof ApiError ? error.message : "Internal Server Error";
+
+    return res.status(statusCode).json({
+      success: false,
+      message,
+      errors: error.errors || [],
+    });
+  }
+};
+
+const logoutUser = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    user.refreshToken = undefined;
+    user.save();
+
+    // await User.findByIdAndUpdate(
+    //   userId,
+    //   {
+    //     $set: {
+    //       refreshToken: undefined,
+    //     },
+    //   },
+    //   {
+    //     new: true,
+    //   }
+    // );
+
+    const cookiesOptions = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    return res
+      .status(200)
+      .clearCookie("accessToken", cookiesOptions)
+      .clearCookie("refreshToken", cookiesOptions)
+      .json(new ApiResponse(200, "user logged out successfully"));
+  } catch (error) {
+    console.error("Error: ", error);
+
+    const statusCode = error instanceof ApiError ? error.statusCode : 500;
+
+    const message =
+      error instanceof ApiError ? error.message : "Internal Server Error";
+
+    return res.status(statusCode).json({
+      success: false,
+      message,
+      errors: error.errors || [],
+    });
+  }
+};
+
+export { registerUser, loginUser, logoutUser };
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,45 @@
+import { ApiError } from "../utils/ApiError.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { User } from "../models/user.model.js";
+dotenv.config({
+  path: "./.env",
+});
+
+export const verifyJWT = async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "unauthorized request");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "invalid access token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Error: ", error);
+
+    const statusCode = error instanceof ApiError ? error.statusCode : 500;
+
+    const message =
+      error instanceof ApiError ? error.message : "Internal Server Error";
+
+    return res.status(statusCode).json({
+      success: false,
+      message,
+      errors: error.errors || [],
+    });
+  }
+};
